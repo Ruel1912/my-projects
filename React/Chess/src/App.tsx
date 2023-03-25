9ee15c54daabb7dca28e6f31a7fe5bb0c9d3bb6d
Mikhail <veselmikhail04@yandex.ru>
2023-03-25 19:57:49 +0300

    Начал шахматы

---
+++ React/Chess/src/App.tsx
@@ -0,0 +1,23 @@
+import React, { useContext, useEffect, useState } from 'react';
+import BoardComponent from './components/BoardComponent';
+import { Board } from './models/Board';
+
+const App = () => {
+  const [board, setBoard] = useState(new Board())
+
+  useEffect(() => restart(), [])
+
+  function restart() {
+    const newBoard = new Board()
+    newBoard.initCells()
+    setBoard(newBoard)
+  }
+  return (
+    <div className='app'>
+      <BoardComponent board={board} setBoard={setBoard} />
+
+    </div>
+  );
+};
+
+export default App;
