5a905e2545f4fb4aa73d376f1dcfa9fe8205b09c
Mikhail Katagarov <mikatagarov@gmail.com>
2024-02-09 15:50:10 +0300

    laravel start

---
+++ Laravel/start/app/Http/Controllers/UserController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+class UserController extends Controller {
+  
+  public function show() {
+    return 'hello world';
+  }
+
+  public function all() {
+    return 'all';
+  }
+}
+?>
